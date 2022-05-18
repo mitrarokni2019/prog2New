ca1bc9e43cc3d7ff3de1283f8d61784001d634df
mitrarokni2019 <miro0545@arrhenius.it.uu.se>
2022-05-18 20:17:10 +0200

    added integer.cpp

--- integer.cpp
+++ integer.cpp
@@ -6,9 +6,12 @@ class Integer{
 		Integer(int);
 		int get();
 		void set(int);
+		int fib();
 	private:
-		int val;
+	int privatefib(int);
+	int val;
 	};
+
  
 Integer::Integer(int n){
 	val = n;
@@ -22,10 +25,21 @@ void Integer::set(int n){
 	val = n;
 	}
 
+int Integer::fib(){
+	return privatefib(val);
+
+}
+
+int Integer::privatefib(int x){
+	if (x <= 1)
+        return x;
+    return privatefib(x-1) + privatefib(x-2);
+	}
 
 extern "C"{
 	Integer* Integer_new(int n) {return new Integer(n);}
 	int Integer_get(Integer* integer) {return integer->get();}
+	int Integer_fib(Integer* integer) {return integer->fib();}
 	void Integer_set(Integer* integer, int n) {integer->set(n);}
 	void Integer_delete(Integer* integer){
 		if (integer){
